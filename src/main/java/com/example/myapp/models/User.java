247cb5f93bab4d6775c02dace878c7b9eed3d18c
Ayush Shukla <ashukla2705@gmail.com>
2018-05-09 23:51:45 -0400

    User model and repository added

---
+++ src/main/java/com/example/myapp/models/User.java
@@ -0,0 +1,49 @@
+package com.example.myapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	private String username;
+	private String password;
+	private String firstname;
+	private String lastname;
+	
+}
---
+++ src/main/java/com/example/myapp/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.myapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
