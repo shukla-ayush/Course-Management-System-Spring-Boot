b901d3a101e9fd3b5cfcb9678a682b7c23a702e4
Ayush Shukla <ashukla2705@gmail.com>
2018-05-30 23:26:29 -0400

    removed imports

--- src/main/java/com/example/myapp/models/Widget.java
+++ src/main/java/com/example/myapp/models/Widget.java
@@ -1,14 +1,10 @@
 package com.example.myapp.models;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
